b184a2e0050c91223a65ec280d08cfa903ae2ecb
Atmoroman <atmoroma@gmail.com>
2026-04-13 13:01:32 +0100

    version3

--- test/face.gd
+++ test/face.gd
@@ -14,9 +14,10 @@ func _process(delta: float) -> void:
 func _draw() -> void:
 	var color = randf_range(0,1)
 	var rand = randf_range(0,1)
-	draw_circle(Vector2(100, 50), radius, Color.from_hsv(color, 1, 1, 0.5), false, 5)
-	draw_circle(Vector2(150, 0), radius/5, Color.from_hsv(color, 1, 1, 0.5), true, 5)
-	draw_circle(Vector2(50, 0), radius/5, Color.from_hsv(color, 1, 1, 0.5), true, 5)
+	var numOfEyes = floor(randf_range(2,7))
+	draw_circle(Vector2(100, 80), radius, Color.from_hsv(color, 1, 1, 0.5), false, 5)
+	for i in range(numOfEyes):
+		draw_circle(Vector2(50 + 30*i, 20), 4, Color.from_hsv(color, 1, 1, 0.5), true, 5)
 	draw_line(Vector2(50, 100), Vector2(150,100), Color.from_hsv(color, 1, 1, 0.5), 2)
 	if(rand>0.5):
 		draw_line(Vector2(50, 100), Vector2(20,50), Color.from_hsv(color, 1, 1, 0.5), 2)
